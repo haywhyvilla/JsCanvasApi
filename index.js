25cf025124abe5291a070ca65190f22a886d2e3a
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-01-10 23:17:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const { Engine, Render, Runner, World, Bodies } = Matter;
+
+const width = 800;
+const height = 600;
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    wireframes: true,
+    width,
+    height,
+  },
+});
+
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+const walls = [
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
+];
+World.add(world, walls);
+
+// const grid = [];
+
+// for (let i = 0; i < 3; i++) {
+//   grid.push([]);
+//   for (let j = 0; j < 3; j++) {
+//     grid[i].push(false);
+//   }
+// }
+
+// console.log(grid);
+
+const grid = Array(3)
+  .fill(null)
+  .map(() => Array(3).fill(false));
